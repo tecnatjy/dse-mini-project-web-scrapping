6eafcaaa87b5209c6ec4b6874325e9f4ce2f2055
jia_yun_tan <jia_yun_tan@dell.com>
2024-02-27 21:09:57 +0800

    created static folder to store images and csv files

--- ads_miniproject/ads_miniproject/forms.py
+++ ads_miniproject/ads_miniproject/forms.py
@@ -3,8 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
-
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
