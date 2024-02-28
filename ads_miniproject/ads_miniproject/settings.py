383bef875839d62dfdf4144e78e9fad442be8c8b
jia_yun_tan <jia_yun_tan@dell.com>
2024-02-28 14:49:51 +0800

    finalize and cleanup all the null cell and commented code

--- ads_miniproject/ads_miniproject/settings.py
+++ ads_miniproject/ads_miniproject/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_URL = 'static/'
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[STATIC_DIR]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
